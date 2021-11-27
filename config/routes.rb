2113d54cfbadf8bb2f4aaf2d84bc8b8c05ffed2a
Tan Valentine <valentinetan91@gmail.com>
2021-11-27 21:17:01 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :coach do
+    resources :lessons
+  end
+
+  resources :sports, only: [:show] do
+    resources :coaches, only: [:index]
+  end
+
+  resources :coaches, only: [:show] do
+    resources :lessons, only: [:index]
+  end
+
+  resources :lessons, only: [:index]
 end
