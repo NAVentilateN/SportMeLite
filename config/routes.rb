735061a9c0ade57b735df2500adf9cb3c0c82d18
Nicholas Chew <chewonithard@gmail.com>
2021-11-28 12:29:07 +0800

    Added more routes for lessons and user

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,24 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # for List of User Lessons page (Coach's journey with toggle button)
   namespace :coach do
     resources :lessons
   end
 
+  # for Find Sport page > Find Coach page
   resources :sports, only: [:index] do
     resources :coaches, only: [:index]
   end
 
+  # for Coach Details page > List of Lessons page
   resources :coaches, only: [:show] do
     resources :lessons, only: [:index]
   end
 
-  resources :lessons, only: [:index]
+  # all 7 CRUD actions required for lessons
+  resources :lessons
+
+  # for Edit Profile page
+  resources :user, only: [:edit, :update]
 end
