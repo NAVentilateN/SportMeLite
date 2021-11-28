9710b9b4313c66f40063bc6ed70fc7e5d4914b6c
daniel tan <dantwq90@gmail.com>
2021-11-28 22:44:14 +0800

    edited lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ApplicationRecord
-  belongs_to :user
-  belongs_to :coach_profile
-  validates :coach_profile, presence: true
+  belongs_to :student_id
+  belongs_to :coach_id
+  validates :coach_id, presence: true
   validates :start_time, :end_time, presence:true
   validates_date :start_time, on: :create, on_or_after: :today
   validates_datetime :end_time, after: :start_time
