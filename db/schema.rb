a59ba22ff544ea4545d726ed60f4758adc5085be
Tan Valentine <valentinetan91@gmail.com>
2021-11-28 11:14:57 +0800

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_27_143037) do
+ActiveRecord::Schema.define(version: 2021_11_28_030914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_11_27_143037) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.date "date_of_birth"
-    t.string "Contact_number"
+    t.string "contact_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
