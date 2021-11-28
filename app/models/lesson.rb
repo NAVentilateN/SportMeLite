3bd4a920d5d2a54542f5a7b5f3867cc22aa04620
daniel tan <dantwq90@gmail.com>
2021-11-28 21:09:42 +0800

    removed validation of student for lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,11 +1,11 @@
 class Lesson < ApplicationRecord
   belongs_to :student_id
   belongs_to :coach_id
-  validates :student, :coach, presence: true
+  validates :coach, presence: true
   validates :start_time, :end_time, presence:true
   validates_date :start_time, on: :create, on_or_after: :today
   validates_datetime :end_time, after: :start_time
   validates :location, presence: true
-  validates :price, numericality: true
-  validates :status, inclusion: { in: [ true, false ] }
+  validates :price, numericality: true, presence: true
+  validates :status, presence: true, inclusion: { in: [ true, false ] }
 end
