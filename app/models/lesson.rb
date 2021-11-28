98250379588c5060cc71279c707dc5d76f394cfe
daniel tan <dantwq90@gmail.com>
2021-11-28 21:01:22 +0800

    added validations for lessons model and sports model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,11 @@
 class Lesson < ApplicationRecord
   belongs_to :student_id
   belongs_to :coach_id
+  validates :student, :coach, presence: true
+  validates :start_time, :end_time, presence:true
+  validates_date :start_time, on: :create, on_or_after: :today
+  validates_datetime :end_time, after: :start_time
+  validates :location, presence: true
+  validates :price, numericality: true
+  validates :status, inclusion: { in: [ true, false ] }
 end
