654437bff95c1f0363365e17b69de5f5d3917a29
daniel tan <dantwq90@gmail.com>
2021-11-29 21:35:20 +0800

    added a Module on first line

--- app/controllers/coach/lessons_controller.rb
+++ app/controllers/coach/lessons_controller.rb
@@ -1,47 +1,49 @@
-class LessonsController < ApplicationController
-  before_action :set_lesson, only: [:show, :edit, :update, :destroy]
+module Coach
+  class LessonsController < ApplicationController
+    before_action :set_lesson, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @lessons = Lesson.where(current_user.id = coach_id)
-  end
-
-  def new
-    # @user = current_user
-    @lesson = Lesson.new
-  end
-
-  def create
-    @lesson = Lesson.new(lesson_params)
-    # @user = current_user
-    @lesson.coach_id = current_user
-    @lesson.status = false
-    if @lesson.save
-      redirect_to action: 'index'
-    else
-      render :new
+    def index
+      @lessons = Lesson.where(current_user.id = coach_id)
     end
-  end
 
-  def edit
-  end
+    def new
+      # @user = current_user
+      @lesson = Lesson.new
+    end
 
-  def update
-    @lesson.update(lesson_params)
-    redirect_to action: 'index'
-  end
+    def create
+      @lesson = Lesson.new(lesson_params)
+      # @user = current_user
+      @lesson.coach_id = current_user
+      @lesson.status = false
+      if @lesson.save
+        redirect_to action: 'index'
+      else
+        render :new
+      end
+    end
 
-  def destroy
-    @lesson.destroy
-    redirect_to action: 'index'
-  end
+    def edit
+    end
 
-  private
+    def update
+      @lesson.update(lesson_params)
+      redirect_to action: 'index'
+    end
 
-   def lesson_params
-    params.require(:lesson).permit(:start_time, :end_time, :location, :price)
-  end
+    def destroy
+      @lesson.destroy
+      redirect_to action: 'index'
+    end
 
-  def set_lesson
-    @lesson = Lesson.find(params[:id])
+    private
+
+    def lesson_params
+      params.require(:lesson).permit(:start_time, :end_time, :location, :price)
+    end
+
+    def set_lesson
+      @lesson = Lesson.find(params[:id])
+    end
   end
 end
