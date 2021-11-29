faa1d09e7378c94cc39d35c4fbcdf8f899a72318
NAVentilateN <valentinetan91@gmail.com>
2021-11-30 00:05:54 +0800

    change user routes to show only

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, except: :index
-  
+  resources :users, only: :show
+
   namespace :coach do
     resources :lessons
   end
