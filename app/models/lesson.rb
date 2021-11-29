aad8d8ab93b531c730b417a6095d57475bbeb3b0
Liwen S <liwenxsu@gmail.com>
2021-11-29 21:26:39 +0800

    updated validation for lessons - start_date_time and end_date_time

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,9 +2,9 @@ class Lesson < ApplicationRecord
   belongs_to :student_id
   belongs_to :coach_id
   validates :coach_id, presence: true
-  validates :start_time, :end_time, presence:true
-  validates_date :start_time, on: :create, on_or_after: :today
-  validates_datetime :end_time, after: :start_time
+  validates :start_date_time, :end_date_time, presence:true
+  validates_date :start_date_time, on: :create, on_or_after: :today
+  validates_datetime :end_date_time, after: :start_date_time
   validates :location, presence: true
   validates :price, numericality: true, presence: true
   validates :status, presence: true, inclusion: { in: [ true, false ] }
