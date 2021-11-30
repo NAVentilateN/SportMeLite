c51dd8fb7b02762955ad35c1dcefb7bc8b7d5beb
NAVentilateN <valentinetan91@gmail.com>
2021-11-30 21:09:44 +0800

    updated user model to include associations. updated navbar to include button to coach page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :coach_profile
+  has_one :sport, through: :coach_profile
+  has_many :lessons
+  has_many :students, through: :lessons
   validates :name, presence: true, length: { minimum: 6 }
   validates_date :date_of_birth, on_or_before: :today
   validates :contact_number, presence: true, length: { minimum: 8 }
