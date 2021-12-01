56fcfe1a9bdb94e84330a7395b9b70a033419dc0
NAVentilateN <valentinetan91@gmail.com>
2021-12-01 20:39:14 +0800

    added profile pic upload to new and edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :contact_number, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :contact_number, :date_of_birth, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :contact_number, :date_of_birth])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :contact_number, :date_of_birth, :photo])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
   has_one :coach_profile
   has_one :sport, through: :coach_profile
   has_many :lessons_to_attend, class_name: 'Lesson', foreign_key: 'student_id', inverse_of: :student
