2875154975e919911238ef565f0092234c1c3ef6
Liwen S <liwenxsu@gmail.com>
2021-12-06 21:35:12 +0800

    small change ILIKE to search in SportsCon

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -1,9 +1,9 @@
 class SportsController < ApplicationController
   def index
-    @query = params['search']['query'].downcase unless params['search'].nil?
+    @query = params['search']['query'] unless params['search'].nil?
     if @query
       @sports_heading = 'Search Results'
-      @sports = Sport.where("name LIKE ?", "%#{@query}%")
+      @sports = Sport.where("name ILIKE ?", "%#{@query}%")
     else
       @sports_heading = 'All Sports'
       @sports = Sport.all
