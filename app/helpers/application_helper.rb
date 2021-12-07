31b37b42efe6e3890ac5bb1b0b7cd47d365ff019
daniel tan <dantwq90@gmail.com>
2021-12-07 14:44:50 +0800

    placed coach_path? method in ApplicationHelper module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def coach_path?
+    params[:controller].match?(/^(coach\/).*/) || current_user.coach_profile && current_page?(user_coach_profile_path(current_user, current_user.coach_profile))
+  end
 end
