add13e0889bb260ae117a150bb71f2256437250a
Nicholas Chew <chewonithard@gmail.com>
2021-12-09 16:47:47 +0800

    added icons to my lessons page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def coach_path?
     params[:controller].match?(/^(coach\/).*/)
   end
+
+  def sport_icon(sport)
+    if sport == 'soccer'
+      'sports_soccer'
+    elsif sport == 'badminton'
+      'sports_tennis'
+    elsif sport == 'tennis'
+      'sports_tennis'
+    elsif sport == 'swimming'
+      'pool'
+    elsif sport == 'basketball'
+      'sports_basketball'
+    end
+  end
 end
