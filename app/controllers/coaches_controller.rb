72f2bc5a7329cdcc08939c52d7e3063b058ce00f
daniel tan <dantwq90@gmail.com>
2021-12-11 14:05:32 +0800

    edited animation and js file

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -12,11 +12,4 @@ class CoachesController < ApplicationController
                                       .select { |lesson| lesson.start_date_time > Date.today }
                                       .sort_by { |lesson| lesson.start_date_time }
   end
-
-  def list_lessons
-    @coach = User.find(params[:id])
-    @lessons = @coach.lessons_to_teach
-                                      .select { |lesson| lesson.start_date_time > Date.today }
-                                      .sort_by { |lesson| lesson.start_date_time }
-  end
 end
