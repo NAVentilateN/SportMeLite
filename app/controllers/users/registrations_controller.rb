8ad82f5a883a62fc120d74716ab86fa87f6d6970
NAVentilateN <valentinetan91@gmail.com>
2021-12-13 20:45:44 +0800

    add check before user destroy account

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,23 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  def destroy
-    if check_active_lessons?
-      redirect_to root_path, alert: "You can't delete your account"
-    else
-      super
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    def destroy
+      if check_active_lessons?
+        redirect_to root_path, alert: "You can't delete your account"
+      else
+        super
+      end
     end
-  end
 
-  private
+    private
 
-  def check_active_lessons?
-    current_user.lessons_to_teach.each do |lesson|
-      return true if lesson.start_date_time > Date.today && lesson.status
+    def check_active_lessons?
+      current_user.lessons_to_teach.each do |lesson|
+        return true if lesson.start_date_time > Date.today && lesson.status
+      end
+
+      current_user.lessons_to_attend.each do |lesson|
+        return true if lesson.start_date_time > Date.today
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :photo
-  has_one :coach_profile
+  has_one :coach_profile, dependent: :destroy
   has_one :sport, through: :coach_profile
   has_many :lessons_to_attend, class_name: 'Lesson', foreign_key: 'student_id', inverse_of: :student
   has_many :lessons_to_teach, class_name: 'Lesson', foreign_key: 'coach_id', inverse_of: :coach
