64e6f00df241c32176d8db91869d133e114bef73
Liwen S <liwenxsu@gmail.com>
2021-12-15 19:58:59 +0800

    added chat route nested within coach

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
   resources :coaches, only: [:show] do
     resources :reviews, only: [:index]
     resources :lessons, only: [:index]
+    resources :chats, only: [:show]
     member do
       get :list_lessons
     end
