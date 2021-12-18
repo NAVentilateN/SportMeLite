7b488a63365214de0e850199824ff0f7d6ca91f8
Nicholas Chew <chewonithard@gmail.com>
2021-12-18 17:31:25 +0800

    debug

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,10 +1,10 @@
 class StripeCheckoutSessionService
   def call(event)
-    order = Order.find_by(checkout_session_id: event.data.object.id)
-    order.update(state: 'paid')
     lesson = Lesson.find(order.lesson_id)
     lesson.student = current_user
     lesson.status = true
     lesson.save!
+    order = Order.find_by(checkout_session_id: event.data.object.id)
+    order.update(state: 'paid')
   end
 end
