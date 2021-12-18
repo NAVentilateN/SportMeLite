e02dd05e6cc57a2418a23eb71d0a23123d60c862
Liwen S <liwenxsu@gmail.com>
2021-12-18 14:42:28 +0800

    add index for chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,13 @@
 class ChatsController < ApplicationController
+  def index
+    @chats = Chat.where(coach: current_user).or(Chat.where(student: current_user))
+  end
+
   def create
     @coach = User.find(params[:coach_id])
     @chat = Chat.new(name: "#{@coach.name}")
+    @chat.coach = @coach
+    @chat.student = current_user
     @chat.save
     redirect_to coach_chat_path(@coach, @chat)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   resources :users, only: :show do
     resources :coach_profiles, only: [:new, :create]
+    resources :chats, only: [:index]
   end
 
 
