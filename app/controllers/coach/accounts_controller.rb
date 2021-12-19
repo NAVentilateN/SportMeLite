bba2025f250acad98cdc4d10b367254f0271e9ba
Nicholas Chew <chewonithard@gmail.com>
2021-12-19 15:43:22 +0800

    css for accounts page

--- app/controllers/coach/accounts_controller.rb
+++ app/controllers/coach/accounts_controller.rb
@@ -2,5 +2,6 @@ class Coach::AccountsController < ApplicationController
   def index
     all_lessons = current_user.lessons_to_teach
     @lessons = all_lessons.sort_by(&:start_date_time)
+    @lessons_with_orders = all_lessons.select(&:order)
   end
 end
