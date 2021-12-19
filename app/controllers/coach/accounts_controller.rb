f529494f62024006e91c8d8f8137714b47f8b482
Nicholas Chew <chewonithard@gmail.com>
2021-12-19 16:25:36 +0800

    fixed date logic for order sorting

--- app/controllers/coach/accounts_controller.rb
+++ app/controllers/coach/accounts_controller.rb
@@ -2,6 +2,6 @@ class Coach::AccountsController < ApplicationController
   def index
     all_lessons = current_user.lessons_to_teach
     @lessons = all_lessons.sort_by(&:start_date_time)
-    @lessons_with_orders = all_lessons.select(&:order)
+    @lessons_with_orders = all_lessons.select(&:order).sort_by { |lesson| lesson.order.created_at }
   end
 end
