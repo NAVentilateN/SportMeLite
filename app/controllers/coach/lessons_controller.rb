a539197920e66a06523e484812ad40eba485b133
Daniel <dantwq90@gmail.com>
2021-12-23 12:30:06 +0800

    added data controllers to student lessons page

--- app/controllers/coach/lessons_controller.rb
+++ app/controllers/coach/lessons_controller.rb
@@ -2,12 +2,12 @@ module Coach
   class LessonsController < ApplicationController
     before_action :set_lesson, only: [:show, :edit, :update, :destroy]
     before_action :authorize_coach, only: [:index, :upcoming, :history]
+    before_action :set_lesson_new, only: [:index, :upcoming, :history, :new]
 
     def index
       all_lessons = current_user.lessons_to_teach
       @lessons = all_lessons.sort_by(&:start_date_time)
       @lessons_json = all_lessons.map { |lesson| lesson.to_json}
-      @lesson = Lesson.new
       respond_to do |format|
         format.html 
         format.text { render partial: 'coach/lessons/lessons_list', formats: [:html]}
@@ -22,7 +22,6 @@ module Coach
     end
 
     def new
-      @lesson = Lesson.new
       @locations = Location.all.uniq.sort_by(&:name)
       respond_to do |format|
         format.html
@@ -97,6 +96,10 @@ module Coach
     def set_lesson
       @lesson = Lesson.find(params[:id])
       authorize([:coach, @lesson])
+    end
+
+    def set_lesson_new
+      @lesson = Lesson.new
     end
   end
 end
