ef5fa0239221f1cdc42e7077e26ebf105e1bcf25
Daniel Tan <dantwq90@gmail.com>
2021-12-29 21:21:50 +0800

    Revert "added filter and sort to coach listing page"

--- db/migrate/20211229130609_add_gender_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddGenderToUser < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :gender, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_29_130609) do
+
+ActiveRecord::Schema.define(version: 2021_12_29_114752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -204,7 +205,6 @@ ActiveRecord::Schema.define(version: 2021_12_29_130609) do
     t.boolean "admin", default: false
     t.string "provider"
     t.string "uid"
-    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
