849180ecb240d903947859c08ab3b76f07157447
NAVentilateN <valentinetan91@gmail.com>
2022-01-03 22:10:47 +0800

    fixed schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_01_01_065047) do
-=======
-ActiveRecord::Schema.define(version: 2021_12_29_130609) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
