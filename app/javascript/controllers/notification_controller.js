912933c83ac58138d5ce589fa6791976ef3c861a
Liwen S <liwenxsu@gmail.com>
2022-01-10 20:05:55 +0800

    notifications refresh WIP

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -27,26 +27,30 @@ export default class extends Controller {
       .then(response => response.json())
       .then((data) => {
         // parse data and replace notificationDetailsTarget with updated allNotifications
-        data.forEach(element => {
-          const sender = element["sender"];
-          const action = element["action"];
-          const url = element["url"];
-          const id = element["id"];
+        console.log(data.length);
+        if (data.length > 0) {
+          // this.notificationDetailsTarget.innerHTML = ``;
+          // const placeholderText = `<span class="dropdown-item">No new notifications.</span>`;
+          // this.notificationDetailsTarget.innerHTML = ``;
+          console.log('there are new messages');
+          data.forEach(element => {
+            const sender = element["sender"];
+            const action = element["action"];
+            const url = element["url"];
+            const id = element["id"];
 
-          if (!data.length == 0) {
             if (!allSenders.includes(sender)) {
               console.log('allsenders:', allSenders);
               console.log('sender:', sender);
               const newNotification = `<a class="dropdown-item" href="${url}" id="${id}" data-sender="${sender}" data-action="click->notification#markRead">${action} ${sender}</a>`;
-              notificationDetails.insertAdjacentHTML('beforeend', newNotification);
+              this.notificationDetailsTarget.insertAdjacentHTML('beforeend', newNotification);
               allSenders.push(sender);
             }
-          }
 
-          // if (allSenders.includes(sender)) {
-          //   console.log("sender alr exists in array", allSenders);
-          //   this.notificationDetailsTarget.innerHTML = allNotifications;
-          // } else {
+            // if (allSenders.includes(sender)) {
+            //   console.log("sender alr exists in array", allSenders);
+            //   this.notificationDetailsTarget.innerHTML = allNotifications;
+            // } else {
             // console.log("added new sender to senders array", allSenders);
             // const newNotification = `<a class="dropdown-item" href="${url}" id="${id}" data-sender="${sender}" data-action="click->notification#markRead">${action} ${sender}</a>`
             // allNotifications += newNotification;
@@ -56,10 +60,12 @@ export default class extends Controller {
             // } else {
             //   allSenders.push(sender);
             // }
-          // }
-        });
+            // }
+          });
+        }
+
         // update unreadCount to length
-        if (data.length == 0) {
+        if (data.length === 0) {
           this.unreadCountTarget.innerText = '';
           this.notificationDetailsTarget.innerHTML = '<span class="dropdown-item">No new notifications.</span>';
         } else {
@@ -69,18 +75,19 @@ export default class extends Controller {
   };
 
   getNewNotifications = () => {
-    console.log('getting new notifications');
+    // console.log('getting new notifications');
     this.loadNotifications();
-    console.log('allSenders:', allSenders);
   };
 
   markRead(event) {
-    console.log("mark notification as read");
+    // console.log("mark notification as read");
     const currentNotif = event.currentTarget.id;
     const sender = event.currentTarget.dataset.sender;
-    const notifHTML = String(event.currentTarget);
+    console.log(sender);
+    const currentNotifHTML = String(event.currentTarget);
     const markReadUrl = `/notifications/${currentNotif}/mark_as_read`
     const senderIndex = allSenders.indexOf(sender);
+    console.log('replacing this:', currentNotifHTML);
 
     fetch(markReadUrl, {
       method: "POST",
@@ -91,10 +98,15 @@ export default class extends Controller {
         // remove sender from array
         if (senderIndex !== -1) {
           allSenders.splice(senderIndex, 1);
+          if (allSenders.length === 0) {
+            this.notificationDetailsTarget.innerHTML = '<span class="dropdown-item">No new notifications.</span>';
+          }
           console.log('sender should be removed:', allSenders);
         }
         // remove substring from allNotifications
-        allNotifications.replace(notifHTML, '');
+        console.log('before:', allNotifications);
+        // allNotifications.replace(currentNotifHTML, '');
+        console.log('replaced dropdown list looks like this:', currentNotifHTML);
       });
   }
 };
