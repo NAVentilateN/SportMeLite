749acbb44041e99d06598ef9b601c03b80fba5aa
Daniel <dantwq90@gmail.com>
2022-01-12 14:54:37 +0800

    changed styles to adopt css variables

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -35,7 +35,7 @@ const initMapbox = () => {
       new mapboxgl.GeolocateControl({
         positionOptions: {
             enableHighAccuracy: true
-        }
+        },
         // When active the map will receive updates to the device's location as it changes.
         trackUserLocation: true,
         // Draw an arrow next to the location dot to indicate which direction the device is heading.
