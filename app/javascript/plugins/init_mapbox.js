fe6373b631d1380a5b98fa4b60b90b81b1f6b545
NAVentilateN <valentinetan91@gmail.com>
2022-01-12 19:29:21 +0800

    resolve initMapbox.js error

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -35,9 +35,9 @@ const initMapbox = () => {
       new mapboxgl.GeolocateControl({
         positionOptions: {
             enableHighAccuracy: true
-        }
+        },
         // When active the map will receive updates to the device's location as it changes.
-        trackUserLocation: true,
+        trackUserLocation: true
         // Draw an arrow next to the location dot to indicate which direction the device is heading.
       })
     );
