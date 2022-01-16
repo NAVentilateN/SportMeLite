26772f8e1f22552c5b966e69033c6bb68f035107
NAVentilateN <valentinetan91@gmail.com>
2022-01-16 23:29:05 +0800

    updating filter list word format

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,18 +5,26 @@ class Location < ApplicationRecord
   validates :name, uniqueness: true
 
   filterrific(
-    available_filters: [:with_sport, :with_price]
+    available_filters: [:with_sport, :with_location]
   )
 
   scope :with_sport, ->(sport) {
     where("sports.name = ?", sport).joins(lessons: { coach: { coach_profile: :sport } })
   }
 
-  scope :with_price, ->(min, max) {
-    where(lesson: { price => min && price <= max }).joins(:lesson)
+  # scope :with_price, ->(min, max) {
+  #   where(lesson: { price => min && price <= max }).joins(:lesson)
+  # }
+
+  scope :with_location, ->(location) {
+    where("locations.name = ?", location)
   }
 
   def self.options_for_sport_select
-    Sport.all.map(&:name)
+     Sport.all.map(&:name).map(&:capitalize)
+  end
+
+  def self.options_for_location_select
+    Location.all.map { |location| [location.name.camelize, location.id] }.sort
   end
 end
