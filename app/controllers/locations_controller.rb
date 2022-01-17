68236373f4f9513fbd7e77b542c2d8f72295d461
NAVentilateN <valentinetan91@gmail.com>
2022-01-18 00:01:10 +0800

    filter only active lessons

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -12,7 +12,7 @@ class LocationsController < ApplicationController
       available_filters: [:with_sport, :with_location],
       sanitize_params: true
     ) || return
-    @locations = @filterrific.find.select { |location| location.lessons.count.positive? }
+    @locations = @filterrific.find.select { |location| location.lessons.select_active_lessons.count.positive? }
 
     @markers = @locations.map do |location|
       {
