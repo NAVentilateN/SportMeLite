611372a5c6c36df575ec12abc5db102abe292335
Daniel <dantwq90@gmail.com>
2022-01-18 00:25:50 +0800

    edited lesson card. edited lesson list in coach and location views to standardise with rest. added a scroll to carousel btn

--- app/javascript/packs/scrollButton.js
+++ app/javascript/packs/scrollButton.js
@@ -1,14 +1,21 @@
 const scrollButtonBehavior = () => {
   const scrollBtn = document.querySelector("#scrollToTopBtmBtn");
+  const homeScrollBtn = document.querySelector("#homeScrollBtn");
   const rootElement = document.documentElement;
   const navBar = document.querySelector(".navbar");
   const footer = document.querySelector("footer");
+  const carouselRow = document.querySelector('.row');
 
   const scrollToTop = () => {
     rootElement.scrollTo({
       top: 0,
       behavior: "smooth",
     });
+  };
+
+  const scrollToBottom = () => {
+    homeScrollBtn.classList.add('hide');
+    carouselRow.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   };
 
   const observeNavBar = (entries, observer) => {
@@ -21,22 +28,13 @@ const scrollButtonBehavior = () => {
     });
   };
 
-  const observeFooter = (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        scrollBtn.classList.add("stickBtn");
-      } else {
-        scrollBtn.classList.remove("stickBtn");
-      }
-    });
-  };
-
   const observer = new IntersectionObserver(observeNavBar);
-  const observer2 = new IntersectionObserver(observeFooter);
   observer.observe(navBar);
-  observer2.observe(footer);
 
   scrollBtn.addEventListener("click", scrollToTop);
+  if (homeScrollBtn) {
+    homeScrollBtn.addEventListener("click", scrollToBottom);
+  }
 };
 
 export default scrollButtonBehavior;
