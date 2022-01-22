09a4ef73b6c902ca505006aa37120e8cbedb34ee
Nicholas Chew <chewonithard@gmail.com>
2022-01-22 12:36:35 +0800

    updated orders seed file

--- db/seeds/orders.rb
+++ db/seeds/orders.rb
@@ -11,8 +11,8 @@ end
     state: "paid",
     amount_cents: rand(2000..5000),
     checkout_session_id: 'cs_test_a1Y8rmMSjqJeMUZNfU2drWfJdGlG7c75WBobJYBV7nvnxAqx7J6mf5lEP' + rand(0..9).to_s,
-    user_id: 51,
-    lesson_id: rand(1..400),
+    user_id: rand(41..50),
+    lesson_id: rand(401..421),
     created_at: rand_time(180.days.ago),
     updated_at: rand_time(180.days.ago)
   })
